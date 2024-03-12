db35230ed1a312249984a83b3257eb1f17a616e4
beomzun <punpon53@naver.com>
2024-03-12 16:16:21 +0900

    initial commit

---
+++ src/main/java/UniFest/UniFestApplication.java
@@ -0,0 +1,13 @@
+package UniFest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniFestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniFestApplication.class, args);
+	}
+
+}
