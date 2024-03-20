2906d35451a9cc1c42242b76efd682bfd89ebd09
ShinHB417 <heebum417@naver.com>
2024-03-20 09:40:21 +0900

    feat : cors 설정 추가

--- src/main/java/UniFest/config/SecurityConfig.java
+++ src/main/java/UniFest/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import UniFest.security.filter.JwtAuthenticationFilter;
 import UniFest.security.filter.JwtExceptionFilter;
 import UniFest.security.filter.JwtVerificationFilter;
 import UniFest.security.jwt.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -27,12 +32,29 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         /**
+         * cors 설정
          * httpbasic 비활성화 -> jwt 사용
          * csrf 비활성화 -> restApi 사용하기때문에 stateless
          * authorizeHttpRequests -> 요청에 대해 authorize하는 것
          * requestMatchers -> 어디로 들어오는 요청인지, 뒤에 어떻게 처리할것인지, permitAll 또는 anyRequest, authenticated
          * sessionManagement() -> sessionCreationPolicy(SessionCreationPolicy.STATELESS)
          */
+        http
+                .cors((cors) -> cors.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration configuration = new CorsConfiguration();
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(10000L);
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                }));
+
         http
                 .csrf((auth) -> auth.disable());
         http
--- src/main/java/UniFest/domain/member/controller/MemberController.java
+++ src/main/java/UniFest/domain/member/controller/MemberController.java
@@ -3,14 +3,13 @@ package UniFest.domain.member.controller;
 import UniFest.domain.member.service.MemberService;
 import UniFest.dto.request.member.MemberSignUpRequest;
 import UniFest.dto.response.Response;
+import UniFest.security.userdetails.MemberDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +22,11 @@ public class MemberController {
     public Response postMember(@Valid @RequestBody MemberSignUpRequest memberSignUpRequest) {
         Long savedId = memberService.createMember(memberSignUpRequest);
         return Response.ofSuccess("OK",savedId);
+    }
+
+    @GetMapping("info")
+    public String testMember(@AuthenticationPrincipal MemberDetails memberDetails){
+        return memberDetails.getEmail() + "\n" + memberDetails.getRole() +
+                "\n" + memberDetails.getUsername() + "\n" + memberDetails.getAuthorities();
     }
 }
