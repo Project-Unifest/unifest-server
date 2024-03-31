77c2b7acf504a0bef40a994132876aceeae7bffc
beomzun <punpon53@naver.com>
2024-04-01 01:26:54 +0900

    feat : 예외 클래스 생성

---
+++ src/main/java/UniFest/exception/FestivalNotFoundException.java
@@ -0,0 +1,11 @@
+package UniFest.exception;
+
+
+import org.springframework.http.HttpStatus;
+
+public class FestivalNotFoundException extends UnifestCustomException{
+
+    public FestivalNotFoundException() {
+        super(HttpStatus.NOT_FOUND, "존재하지 않는 축제입니다.", 4001);
+    }
+}
