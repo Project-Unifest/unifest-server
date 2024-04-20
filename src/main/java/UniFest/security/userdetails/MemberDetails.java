d0b5da7075067bb229394a203c334da35f2649ad
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-04-20 14:36:09 +0900

    fix: role의 Normal을 Pending과 Denied로 구분2

--- src/main/java/UniFest/security/userdetails/MemberDetails.java
+++ src/main/java/UniFest/security/userdetails/MemberDetails.java
@@ -17,14 +17,15 @@ public class MemberDetails implements UserDetails {
     private String password;
     private String role;
 
-    public MemberDetails(Long memberId,String email, String password, String role){
+    public MemberDetails(Long memberId, String email, String password, String role){
         this.memberId = memberId;
         this.email = email;
         this.password = password;
         this.role = role;
     }
 
-    private final List<GrantedAuthority> NORMAL_ROLES = AuthorityUtils.createAuthorityList("ROLE_NORMAL");
+    private final List<GrantedAuthority> PENDING_ROLES = AuthorityUtils.createAuthorityList("ROLE_PENDING");
+    private final List<GrantedAuthority> DENIED_ROLES = AuthorityUtils.createAuthorityList("ROLE_DENIED");
     private final List<GrantedAuthority> VERIFIED_ROLES = AuthorityUtils.createAuthorityList("ROLE_VERIFIED");
     private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_ADMIN");
     @Override
@@ -33,7 +34,8 @@ public class MemberDetails implements UserDetails {
     }
 
     private Collection<? extends GrantedAuthority> createAuthorities(String role) {
-        if (role.equals("NORMAL") ) return NORMAL_ROLES;
+        if (role.equals("PENDING") ) return PENDING_ROLES;
+        else if (role.equals("DENIED")) return DENIED_ROLES;
         else if (role.equals("ADMIN")) return ADMIN_ROLES;
         else return VERIFIED_ROLES;
     }
