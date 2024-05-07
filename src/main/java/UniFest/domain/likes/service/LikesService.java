6653f308af5c4d21bc4bc836a599127ad2fc6cc6
onaeonae1 <onaeonae2@gmail.com>
2024-05-07 12:37:55 +0900

    feat: add GET /api/likes?token for getting user-liked boothList

--- src/main/java/UniFest/domain/likes/service/LikesService.java
+++ src/main/java/UniFest/domain/likes/service/LikesService.java
@@ -4,10 +4,14 @@ import UniFest.domain.booth.entity.Booth;
 import UniFest.domain.booth.repository.BoothRepository;
 import UniFest.domain.likes.entity.Likes;
 import UniFest.domain.likes.repository.LikesRepository;
+import UniFest.dto.response.booth.BoothResponse;
 import UniFest.exception.booth.BoothNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -50,5 +54,16 @@ public class LikesService {
 
     }
 
+    public List<BoothResponse> getLikedBooths(String token){
+        List<BoothResponse> boothResponses = new ArrayList<>();
+        List<Likes> likesList = likesRepository.findAllLikesByToken(token);
+        List<Booth> booths = boothRepository.findBoothsByIdIn(
+                likesList.stream().map(like -> like.getBooth().getId()).toList()
+        );
+        for(Booth booth : booths){
+            boothResponses.add(new BoothResponse(booth));
+        }
+        return boothResponses;
+    }
 
 }
