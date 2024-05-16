66f281069dfac88fdeeb8ed708ec9b2e739ceab2
beomzun <punpon53@naver.com>
2024-05-16 13:32:00 +0900

    feat: 등록된 연예인 전체 조회 기능

--- src/main/java/UniFest/domain/star/repository/StarRepository.java
+++ src/main/java/UniFest/domain/star/repository/StarRepository.java
@@ -1,8 +1,14 @@
 package UniFest.domain.star.repository;
 
 import UniFest.domain.star.entity.Star;
+import UniFest.dto.response.star.StarInfo;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StarRepository extends JpaRepository<Star, Long> {
+
+    @Query("select new UniFest.dto.response.star.StarInfo(s.id, s.name, s.img) from Star s")
+    List<StarInfo> findAllStars();
 
 }
