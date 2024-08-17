9ba16fece146dec0d8bc83560d0e6d85420e72d3
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-08-18 02:19:51 +0900

    fix: add NoArgsConstructor to RequestDto

--- src/main/java/UniFest/dto/request/announcement/FestivalInterestRequest.java
+++ src/main/java/UniFest/dto/request/announcement/FestivalInterestRequest.java
@@ -2,9 +2,11 @@ package UniFest.dto.request.announcement;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class FestivalInterestRequest {
     private String FcmToken;
 }
