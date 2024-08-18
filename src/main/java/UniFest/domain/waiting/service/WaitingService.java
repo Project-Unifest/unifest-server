5c914fa954e19cd9edfb08c54142053d7cb026e6
onaeonae1 <onaeonae2@gmail.com>
2024-08-18 15:09:30 +0900

    test: make waitingList fetchtype eager

--- src/main/java/UniFest/domain/waiting/service/WaitingService.java
+++ src/main/java/UniFest/domain/waiting/service/WaitingService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
