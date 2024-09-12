f5e6060fd210378c9cda7ed4d2dba791886aeb74
Roysung0625 <roy06255@gmail.com>
2024-09-12 16:42:48 +0900

    fix: set stamp max size

--- src/main/java/UniFest/domain/stamp/service/StampService.java
+++ src/main/java/UniFest/domain/stamp/service/StampService.java
@@ -5,18 +5,24 @@ import UniFest.domain.booth.repository.BoothRepository;
 import UniFest.domain.stamp.entity.Stamp;
 import UniFest.domain.stamp.repository.StampRepository;
 import UniFest.exception.stamp.StampAlreadyAddedException;
+import UniFest.exception.stamp.StampLimitException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class StampService {
 
     private final StampRepository stampRepository;
     private final BoothRepository boothRepository;
 
+    private int stampLimit = 5;
+
+    @Transactional
     public int addStamp(Long boothId, String token){
         Booth booth = boothRepository.getReferenceById(boothId);
 
@@ -24,6 +30,9 @@ public class StampService {
 
         if (stampResult != null) {
             throw new StampAlreadyAddedException();
+        }
+        if (isMaxStamp(token)){
+            throw new StampLimitException();
         }
         stampResult = new Stamp(booth, token);
         stampRepository.save(stampResult);
@@ -35,5 +44,13 @@ public class StampService {
     public int getStamp(String token){
         List<Stamp> byTokenList = stampRepository.findByToken(token);
         return byTokenList.size();
+    }
+
+    private boolean isMaxStamp(String token){
+        List<Stamp> stampByTokenList = stampRepository.findByToken(token);
+        if(stampByTokenList.size() >= stampLimit){
+            return true;
+        }
+        return false;
     }
 }
