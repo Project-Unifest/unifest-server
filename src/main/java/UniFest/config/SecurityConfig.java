57528ca7b9ee5e27dcf1764fb9a5d4569623f814
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-09-18 23:46:10 +0900

    update: megaphone 발행 admin만 가능하게 수정

--- src/main/java/UniFest/config/SecurityConfig.java
+++ src/main/java/UniFest/config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig{
                         .requestMatchers(HttpMethod.POST, "/festival").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/school").hasRole("ADMIN")
                         .requestMatchers("/megaphone/subscribe").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/megaphone").hasRole("VERIFIED")
+                        .requestMatchers(HttpMethod.POST, "/megaphone").hasRole("ADMIN") // 확성기 메세지 등록은 ADMIN 만
                         .requestMatchers(HttpMethod.PATCH,"/members/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/members").hasRole("ADMIN") // 전체 멤버 조회는 ADMIN만
                         .requestMatchers(HttpMethod.GET, "/api/booths").permitAll()
