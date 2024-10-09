4af9dc286ba4a73a5dbe1992c971f364218c092d
Roysung0625 <roy06255@gmail.com>
2024-10-09 23:14:23 +0900

    fix: App 담당자 요구사항에 맞는 ResponseCode 적용

--- src/main/java/UniFest/domain/stamp/controller/StampController.java
+++ src/main/java/UniFest/domain/stamp/controller/StampController.java
@@ -4,6 +4,7 @@ import UniFest.domain.booth.service.BoothService;
 import UniFest.domain.stamp.service.StampService;
 import UniFest.dto.request.stamp.StampRequest;
 import UniFest.dto.response.Response;
+import UniFest.dto.response.TempResponse;
 import UniFest.dto.response.booth.BoothResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -21,27 +22,27 @@ public class StampController {
 
     @Operation(summary = "Stamp 추가")
     @PostMapping()
-    public Response<Integer> addStamp(@RequestBody StampRequest stampRequest){
+    public TempResponse<Integer> addStamp(@RequestBody StampRequest stampRequest){
         int totalStamp = stampService.addStamp(stampRequest.getBoothId(), stampRequest.getToken());
-        return Response.ofSuccess("OK", Integer.valueOf(totalStamp));
+        return TempResponse.ofSuccess("OK", Integer.valueOf(totalStamp));
     }
 
     @Operation(summary = "Stamp 조회 (token별)")
     @GetMapping()
-    public Response<Integer> getStamp(@RequestParam String token){
-        return Response.ofSuccess("OK", Integer.valueOf(stampService.getStamp(token)));
+    public TempResponse<Integer> getStamp(@RequestParam String token){
+        return TempResponse.ofSuccess("OK", Integer.valueOf(stampService.getStamp(token)));
     }
 
     @Operation(summary = "스탬프 기능 활성화 된 부스 확인")
     @GetMapping("/{festival-id}")
-    public Response<List<BoothResponse>> getStampBooths(@PathVariable("festival-id") Long festivalId){
+    public TempResponse<List<BoothResponse>> getStampBooths(@PathVariable("festival-id") Long festivalId){
         List<BoothResponse> boothList = boothService.getStampEnabledBooths(festivalId);
-        return Response.ofSuccess("OK", boothList);
+        return TempResponse.ofSuccess("OK", boothList);
     }
 
     @Operation(summary = "Booth StampEnabled 바꾸기(toggle)")
     @GetMapping("/{booth-id}/stampEnabled")
-    public Response<Boolean> switchStampEnabled(@PathVariable("booth-id") Long boothId){
-        return Response.ofSuccess("OK", boothService.updateStampEnabled(boothId));
+    public TempResponse<Boolean> switchStampEnabled(@PathVariable("booth-id") Long boothId){
+        return TempResponse.ofSuccess("OK", boothService.updateStampEnabled(boothId));
     }
 }
