bfbd320a65ac94175e94927bac1a7918d79bca31
onaeonae1 <onaeonae2@gmail.com>
2025-01-19 22:47:19 +0900

    fix: correct annotations for sync service

--- src/main/java/UniFest/domain/sync/service/SyncService.java
+++ src/main/java/UniFest/domain/sync/service/SyncService.java
@@ -4,7 +4,15 @@ package UniFest.domain.sync.service;
 import UniFest.domain.Device;
 import UniFest.domain.sync.repository.SyncRepository;
 import UniFest.dto.request.sync.PostSyncRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j
 public class SyncService {
     private final SyncRepository syncRepository;
 
