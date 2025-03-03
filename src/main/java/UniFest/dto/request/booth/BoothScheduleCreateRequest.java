734ae18db09302a386d9b1acab3435cac922a696
Roysung0625 <roy06255@gmail.com>
2025-03-03 15:49:04 +0900

    fix: 표기오류 수정

--- src/main/java/UniFest/dto/request/booth/BoothScheduleCreateRequest.java
+++ src/main/java/UniFest/dto/request/booth/BoothScheduleCreateRequest.java
@@ -1,6 +1,7 @@
 package UniFest.dto.request.booth;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -14,9 +15,11 @@ public class BoothScheduleCreateRequest {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     LocalDate date;
     @NotNull
+    @Schema(example = "HH:mm:ss")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss", timezone = "Asia/Seoul")
     LocalTime openTime;
     @NotNull
+    @Schema(example = "HH:mm:ss")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss", timezone = "Asia/Seoul")
     LocalTime closeTime;
 }
